19c231d7d9d65cc50f0cb303acc359a282fe74cd
fahmisalman <fahmisalman@gmail.com>
2018-10-29 12:06:58 +0700

    Modified

--- Summarizer.py
+++ Summarizer.py
@@ -42,24 +42,8 @@ def word_freq(data):
         res[word] = w.count(word)
     return res
 
-if __name__ == '__main__':
 
-    paragraph = '''Like a bowling ball on a skating rink, the black geodesic sphere of the East Greenland Ice-Core Project’s communal living space stands out against the endless white nothingness of the Greenland ice sheet.
-
-    But the real action at East GRIP is under the surface. Researchers are drilling through more than 2.5 kilometers of ice, down to the bedrock below. The ice is sliding fast — for a glacier — toward the sea. Scientists here want to know why. The answer may hold clues to the future of the world’s coastal cities.
-
-    Greenland is melting. As it melts, it adds roughly 1 millimeter of water per year to global sea levels. And the pace of melting is quickening.
-
-    If all the ice covering the world’s largest island were to thaw, sea levels would rise roughly 6 meters. Scientists don’t know how fast, or how likely, that is to happen. East GRIP is looking for evidence to inform both those questions.
-
-    The answers are a matter of growing urgency. The seas are rising faster. And the same processes at work on Greenland’s glaciers at the top of the world could send vast sections of Antarctica’s ice sheet into the sea as well, raising ocean levels even further.
-
-    
-    The Arctic is warming twice as fast as the rest of the planet. Scientists studying the rapid changes gather in the small Greenland town of Kangerlussuaq, a former U.S. military base built during World War II. Through the Cold War, this outpost supplied remote radar sites watching for a nuclear attack coming over the pole.
-
-    These days, military transport planes fly scientists and their equipment across 1,000 kilometers of Arctic ice to East GRIP. They make research possible here and at other far-flung scientific outposts on the vast Greenland ice sheet.
-
-    Departing from Kangerlussuaq, VOA visited East GRIP and other remote corners of Greenland with the 109th Airlift Wing of the U.S. Air National Guard for a firsthand look at science in action at the leading edge of climate change.'''
+def summarizer(paragraph):
 
     sentence_list = sentence_split(paragraph)
     data = []
@@ -78,8 +62,31 @@ if __name__ == '__main__':
 
     sortList = sorted(range(len(ranking)),key=ranking.__getitem__, reverse=True)
     n = 3
+    sentence = ''
     for i in range(n):
-        print('{}.'.format(sentence_list[sortList[i]]), end=' ')
-    print()
+        sentence += '{}. '.format(sentence_list[sortList[i]])
+    return sentence
 
-'''https://www.analyticsvidhya.com/blog/2015/10/beginner-guide-web-scraping-beautiful-soup-python/'''
+    '''https://www.analyticsvidhya.com/blog/2015/10/beginner-guide-web-scraping-beautiful-soup-python/'''
+
+
+if __name__ == '__main__':
+
+    paragraph = '''Penonton Kejuaraan Dunia Motocross MXGP Disarankan Beli Tiket Online
+Jakarta - Gelaran Kejuaraan Dunia Motorcross MXGP di Pangkalpinang tinggal 11 hari lagi. Agar tak ketinggalan kesempatan untuk menyaksikan langsung, penonton disarankan untuk memesan tiket secara online. 
+
+Kejuaraan Dunia Motorcross MXGP yang merupakan seri kedua itu akan dihelat di area Sport Center GOR Sahabudin, Pangkalpinang, Bangka Belitung. Agenda tersebut digeber selama dua hari, Sabtu (4/3/2017) dan Minggu (5/3/2017). Selain MXGP, dalam ajang itu juga digeber kategori MX2, dan MX Women. 
+
+Untuk bisa menyaksikan secara langsung aksi para crosser dunia itu, panitia penyelenggara menawarkan tiket dengan berbagai pilihan. Yakni, kelas VIP seharga Rp 3,5 juta. Pemilik tiket bisa nonton aksi para crosses dalam dua hari sekaligus mendapatkan konsumsi saat perlombaan. 
+
+Kemudian tiket tribun yang diberi banderol harga Rp 700 ribu. Bedanya, penonton kategori ini tak mendapatkan konsumsi. 
+
+"Kategori ketiga adalah kelas festival. Harganya murah-murah saja, per lembar tiket dihargai Rp 110 ribu berlaku per hari," kata A. Judiarto, perwakilan promotor MXGP Indonesia yang juga ketua umum PP IMI DKI Jakarta di kantor detikcom, Rabu (22/2/2017). 
+
+Selain bisa menyaksikan tiga kategori balapan itu, pemilik tiket juga dapat melihat langsung aksi freestyle motocross oleh Alex Martin, Alastair James Sarer, dan William Van de Putte. 
+
+"Mereka akan membuat atraksi-atraksi memukau dari awal sampai akhir," tutur Judiarto. 
+
+"Agar tak kehabisan tiket untuk menikmati itu semua, sebaiknya penonton memesan tiket presale yang dibuka sampai 28 Februari," imbuh dia. '''
+
+    print(summarizer(paragraph))
