31b80c9f7379c6b5cfba09b09bad459d324e3613
fahmisalman <fahmisalman@gmail.com>
2019-01-16 10:47:24 +0700

    Fix code

--- Model/App.py
+++ Model/App.py
@@ -5,12 +5,7 @@ from Model.Web_scraper import NewsScraper
 def fit(url):
 
     news = NewsScraper(url)
-    result = {}
-
-    result['author'] = news.get_author()
-    result['title'] = news.get_title()
-    result['article'] = news.get_article()
-    result['publish date'] = news.get_publish_date()
-    result['summarizer'] = Summarizer.fit(news.get_article())
+    result = {'author': news.get_author(), 'title': news.get_title(), 'article': news.get_article(),
+              'publish date': news.get_publish_date(), 'summarizer': Summarizer.fit(news.get_article())}
 
     return result
